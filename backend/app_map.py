67b0c4326150687925e5c958858cefe98fe942bc
gsw945 <gogs@fake.local>
2018-11-16 11:07:20 +0800

    添加admin页面模板

--- backend/app_map.py
+++ backend/app_map.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 '''（子）应用路径映射'''
 from .apps.home.main import home_app
+from .apps.admin.main import admin_app
 
 
 blueprints = [
-    ('', home_app)
+    ('/', home_app),
+    ('/admin', admin_app)
 ]
---
+++ backend/apps/admin/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''站点首页视图'''
+from flask import render_template
+
+from . import admin_app, app_name
+
+
+@admin_app.route('/')
+def view_index():
+    return render_template(app_name + '/pages/index.html', page_id='view-index')
+
+@admin_app.route('/test/blank')
+def view_test_blank():
+    return render_template(app_name + '/pages/test-blank.html', page_id='view-test-blank')
