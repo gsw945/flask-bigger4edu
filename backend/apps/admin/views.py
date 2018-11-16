f662db0899f93da4450b46ab897c00630c520d8d
gsw945 <gogs@fake.local>
2018-11-16 14:09:51 +0800

    添加登录页面

--- backend/apps/admin/views.py
+++ backend/apps/admin/views.py
@@ -12,3 +12,7 @@ def view_index():
 @admin_app.route('/test/blank')
 def view_test_blank():
     return render_template(app_name + '/pages/test-blank.html', page_id='view-test-blank')
+
+@admin_app.route('/auth/login')
+def view_auth_login():
+    return render_template(app_name + '/pages/auth-login.html', page_id='view-auth-login')
